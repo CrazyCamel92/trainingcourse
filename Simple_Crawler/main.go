1e0aedee8d61354e833e8fc05ecb1660d1759eed
CrazyCamel92 <erez.levi.p@gmail.com>
2017-04-14 23:59:15 +0300

    stuff

---
+++ Simple_Crawler/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+	"html"
+)
+
+type Link struct {
+	url string
+	text string
+	depth int
+}
+
+type HttpError struct {
+	original string
+}
+
+func LinkReader(resp *http.Response,depth int)[] Link  {
+	page := NewTo
+}
+
+func main() {
+
+}
